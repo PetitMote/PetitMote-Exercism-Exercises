12fab852d2fd51a3b549275b5ba556419af37e35
Moté <automates@lepouete.fr>
2021-05-05 18:48:52 +0200

    Change to only list comprehensions - beware of short code !

--- python/Main track/05-matrix/matrix.py
+++ python/Main track/05-matrix/matrix.py
@@ -1,18 +1,15 @@
 class Matrix:
-    """Object to create matrices using a string to represent the 05-matrix"""
+    """Object to create matrices using a string to represent the matrix."""
 
     def __init__(self, matrix_string: str):
-        """At the init, the object transform the string in a 05-matrix using lists
+        """At the init, the object transform the string in a matrix using lists.
 
         :param matrix_string: String representing the 05-matrix, lines separated by "\n" and rows separated by a space.
         """
-        lines_strings = matrix_string.splitlines()
-        self.matrix = [line.split(" ") for line in lines_strings]
-        for i in range(0, len(self.matrix)):
-            self.matrix[i] = [int(n) for n in self.matrix[i]]
+        self.matrix = [[int(n) for n in line.split()] for line in matrix_string.splitlines()]
 
     def row(self, index: int) -> list[int]:
-        """Get a row from the 05-matrix
+        """Get a row from the matrix.
 
         :param index: Index of the row
         :return: The row as a list
@@ -20,12 +17,9 @@ class Matrix:
         return self.matrix[index - 1]
 
     def column(self, index: int) -> list[int]:
-        """Get a column from the 05-matrix
+        """Get a column from the matrix.
 
         :param index: Index of the column
         :return: The column as a list
         """
-        column: list[int] = []
-        for line in self.matrix:
-            column.append(line[index - 1])
-        return column
+        return [line[index - 1] for line in self.matrix]
