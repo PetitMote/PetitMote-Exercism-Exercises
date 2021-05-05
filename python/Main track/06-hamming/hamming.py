16eced9fe4e428a926352ed8de3ae992417b66e9
Moté <automates@lepouete.fr>
2021-05-05 21:26:16 +0200

    Added the 6th exercise, 1st iteration

---
+++ python/Main track/06-hamming/hamming.py
@@ -0,0 +1,9 @@
+# Learned about the zip function and that I should call maths functions on booleans
+
+def distance(strand_a, strand_b):
+    if len(strand_a) != len(strand_b):
+        raise ValueError("Strings are not the same length")
+    # Found on the internet - I guess it's more Pythonic than the intuitive answer I found (before finding this)
+    # Though, I don't like depending on the internal mechanic that translates True to 1
+    nb_errors = sum([a != b for a, b in zip(strand_a, strand_b)])
+    return nb_errors
