0075541edd0a5c6d86b44cba6d01a838369d9428
Moté <automates@lepouete.fr>
2021-05-06 21:16:08 +0200

    Used list comprehension

--- python/Side exercises/Easy/armstrong-numbers/armstrong_numbers.py
+++ python/Side exercises/Easy/armstrong-numbers/armstrong_numbers.py
@@ -1,6 +1,4 @@
 def is_armstrong_number(number: int) -> bool:
-    digits_sum: int = 0
     number_len = len(str(number))
-    for i in str(number):
-        digits_sum += int(i) ** number_len
+    digits_sum = sum(int(i) ** number_len for i in str(number))
     return digits_sum == number
