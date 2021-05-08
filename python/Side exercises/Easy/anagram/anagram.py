946230f768839fba04205556902b17f917fad878
Moté <automates@lepouete.fr>
2021-05-08 21:04:10 +0200

    Corrected a type hint

--- python/Side exercises/Easy/anagram/anagram.py
+++ python/Side exercises/Easy/anagram/anagram.py
@@ -1,4 +1,4 @@
-def find_anagrams(word: str, candidates: list[str]) -> list(str):
+def find_anagrams(word: str, candidates: list[str]) -> list[str]:
     return [candidate for candidate in candidates if check_anagrams(word, candidate)]
 
 
