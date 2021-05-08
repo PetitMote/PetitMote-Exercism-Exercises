a29ecd9832d99a56b1a017025f1789fd59e7a544
Moté <automates@lepouete.fr>
2021-05-08 19:44:21 +0200

    Added anagram side exercise

---
+++ python/Side exercises/Easy/anagram/anagram.py
@@ -0,0 +1,11 @@
+def find_anagrams(word: str, candidates: list[str]) -> list(str):
+    return [candidate for candidate in candidates if check_anagrams(word, candidate)]
+
+
+def check_anagrams(word1: str, word2: str) -> bool:
+    if word1.upper() == word2.upper():
+        return False
+    elif sorted(word1.upper()) == sorted(word2.upper()):  # It's really frustrating that I didn't find this by myself
+        return True
+    else:
+        return False
