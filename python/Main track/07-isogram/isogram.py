f7d5a8ed66ac47b0630fa44a63d773cafc8d9235
Moté <automates@lepouete.fr>
2021-05-11 21:06:33 +0200

    Added a dot to the docstring.

--- python/Main track/07-isogram/isogram.py
+++ python/Main track/07-isogram/isogram.py
@@ -3,7 +3,7 @@ import string
 
 
 def is_isogram(word: str) -> bool:
-    """Return true if the word is an isogram (only 1 occurence of each letter used"""
+    """Return true if the word is an isogram (only 1 occurence of each letter used."""
     word = word.upper()  # So we don't have to manage lower and upper case
     word = "".join(re.findall("[A-Z]", word))  # Remove everything else than uppercase letters
     alphabet = list(string.ascii_uppercase)
