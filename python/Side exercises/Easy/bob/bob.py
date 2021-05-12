a58536b727da034ed8952d0bd6929765baff2e83
Moté <automates@lepouete.fr>
2021-05-12 16:36:12 +0200

    Minor change to use endswith() method

--- python/Side exercises/Easy/bob/bob.py
+++ python/Side exercises/Easy/bob/bob.py
@@ -2,11 +2,11 @@ import re
 
 
 def response(hey_bob: str):
-    hey_bob = re.sub("\s", "", hey_bob)
+    hey_bob = re.sub("\s", "", hey_bob)  # I could use hey_bob.strip(" \n\t\r"). Is the regex cleaner ?
     if not hey_bob:
         answer = "Fine. Be that way!"
     else:
-        question = hey_bob[-1] == "?"
+        question = hey_bob.endswith("?")  # Better than a slice ? Looks cleaner, but I don't know
         caps = hey_bob.isupper()
         # I could condense the conditions on one line, but isn't it more readable like that ?
         if question and caps:
