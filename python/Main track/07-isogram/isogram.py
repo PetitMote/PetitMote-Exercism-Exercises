a788c3caabebdb4e878a88c67609aea30c4e8b80
Moté <automates@lepouete.fr>
2021-05-15 19:46:47 +0200

    Second iteration of the isogram exercise - minor improvements

--- python/Main track/07-isogram/isogram.py
+++ python/Main track/07-isogram/isogram.py
@@ -1,12 +1,15 @@
+# Thanks to @bobahop for the casefold method (better handling of special letters) and for remembering me of using a
+# set instead of a list (minor performance improvement)
+
 import re
 import string
 
 
 def is_isogram(word: str) -> bool:
     """Return true if the word is an isogram (only 1 occurence of each letter used."""
-    word = word.upper()  # So we don't have to manage lower and upper case
-    word = "".join(re.findall("[A-Z]", word))  # Remove everything else than uppercase letters
-    alphabet = list(string.ascii_uppercase)
+    word = word.casefold()  # So we don't have to manage lower and upper case
+    word = "".join(re.findall("[a-z]", word))  # Remove everything else than uppercase letters
+    alphabet = set(string.ascii_lowercase)
 
     for letter in word:
         if letter in alphabet:
