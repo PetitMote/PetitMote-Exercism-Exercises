0bff66341b31ba4861aadd6c3c6b8ec343aff85a
Moté <automates@lepouete.fr>
2021-05-16 16:29:30 +0200

    Isogram exercise : based on set and len

--- python/Main track/07-isogram/isogram.py
+++ python/Main track/07-isogram/isogram.py
@@ -1,20 +1,29 @@
-# Thanks to @bobahop for the casefold method (better handling of special letters) and for remembering me of using a
-# set instead of a list (minor performance improvement)
+# Thanks to @bobahop for the review and the set tip
+# Thanks to @adrianmcli for the filter() based solution :
+# https://exercism.io/tracks/python/exercises/isogram/solutions/f1b6f252568d41be94b063db3d1a234e
 
 import re
-import string
+# import string
 
 
 def is_isogram(word: str) -> bool:
     """Return true if the word is an isogram (only 1 occurence of each letter used."""
     word = word.casefold()  # So we don't have to manage lower and upper case
     word = "".join(re.findall("[a-z]", word))  # Remove everything else than uppercase letters
-    alphabet = set(string.ascii_lowercase)
+    # Alternative found on the community solutions :
+    # word = filter(str.isalpha(), word)
+    if len(word) == len(set(word)):
+        return True
+    else:
+        return False
 
-    for letter in word:
-        if letter in alphabet:
-            alphabet.remove(letter)
-        else:
-            return False
-
-    return True
+    # Old solution
+    # alphabet = set(string.ascii_lowercase)
+    #
+    # for letter in word:
+    #     if letter in alphabet:
+    #         alphabet.remove(letter)
+    #     else:
+    #         return False
+    #
+    # return True
