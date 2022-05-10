119d6edb06916a307640a8d335d7b27a0f97e78a
Moté <automates@lepouete.fr>
2022-05-10 19:26:37 +0200

    JS: exercise 12

---
+++ javascript/12-lucky-numbers/lucky-numbers.js
@@ -0,0 +1,38 @@
+// @ts-check
+
+/**
+ * Calculates the sum of the two input arrays.
+ *
+ * @param {number[]} array1
+ * @param {number[]} array2
+ * @returns {number} sum of the two arrays
+ */
+export function twoSum(array1, array2) {
+    return Number(array1.join("")) + Number(array2.join(""));
+}
+
+/**
+ * Checks whether a number is a palindrome.
+ *
+ * @param {number} value
+ * @returns {boolean}  whether the number is a palindrome or not
+ */
+export function luckyNumber(value) {
+    let valueString = String(value);
+    let valueReverse = Array.from(valueString).reverse().join("");
+    return valueString === valueReverse;
+}
+
+/**
+ * Determines the error message that should be shown to the user
+ * for the given input value.
+ *
+ * @param {string|null|undefined} input
+ * @returns {string} error message
+ */
+export function errorMessage(input) {
+    if (Number(input)) {
+        return "";
+    } else if (!input) return "Required field";
+    else return "Must be a number besides 0";
+}
