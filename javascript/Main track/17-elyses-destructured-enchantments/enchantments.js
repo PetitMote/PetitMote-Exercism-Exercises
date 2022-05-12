67c914cebd14a1632297f17452a8aec9b4912ed9
Moté <automates@lepouete.fr>
2022-05-12 08:13:11 +0200

    JS: exercise 17

---
+++ javascript/Main track/17-elyses-destructured-enchantments/enchantments.js
@@ -0,0 +1,62 @@
+/// <reference path="./global.d.ts" />
+// @ts-check
+
+/**
+ * Get the first card in the given deck
+ *
+ * @param {Card[]} deck
+ *
+ * @returns {Card} the first card in the deck
+ */
+export function getFirstCard([firstCard]) {
+    return firstCard;
+}
+
+/**
+ * Get the second card in the given deck
+ *
+ * @param {Card[]} deck
+ *
+ * @returns {Card} the second card in the deck
+ */
+export function getSecondCard([,secondCard]) {
+    return secondCard;
+}
+
+/**
+ * Switch the position of the first two cards in the given deck
+ *
+ * @param {Card[]} deck
+ *
+ * @returns {Card[]} new deck with reordered cards
+ */
+export function swapTopTwoCards([first, second, ...others]) {
+    return [second, first, ...others];
+}
+
+/**
+ * Put the top card of the given deck into a separate discard pile
+ *
+ * @param {Card[]} deck
+ *
+ * @returns {[Card, Card[]]} the top card of the given
+ * deck and a new deck containing all the other cards
+ */
+export function discardTopCard([first, ...others]) {
+    return [first, others];
+}
+
+/** @type Card[] **/
+const FACE_CARDS = ['jack', 'queen', 'king'];
+
+/**
+ * Insert face cards into the given deck
+ *
+ * @param {Card[]} deck
+ *
+ * @returns {Card[]} new deck where the second,
+ * third, and fourth cards are the face cards
+ */
+export function insertFaceCards([first, ...others]) {
+    return [first, ...FACE_CARDS, ...others];
+}
